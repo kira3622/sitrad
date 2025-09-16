efe53b35043a934dbd1fb99558e74fc93caf97f0
kira3622 <affifyassine3@gmail.com>
2025-09-16 19:38:11 +0100

    Fix: Désactivation de date_hierarchy pour compatibilité PostgreSQL

--- stock/admin.py
+++ stock/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import MouvementStock
 
-admin.site.register(MouvementStock)
+@admin.register(MouvementStock)
+class MouvementStockAdmin(admin.ModelAdmin):
+    list_display = ('matiere_premiere', 'quantite', 'type_mouvement', 'date_mouvement')
+    list_filter = ('type_mouvement', 'matiere_premiere')
+    search_fields = ('matiere_premiere__nom',)
+    # date_hierarchy = 'date_mouvement'
+    list_select_related = ('matiere_premiere',)
