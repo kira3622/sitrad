7bf0cac353743fe1e4aca16b55b176143d28216c
kira3622 <affifyassine3@gmail.com>
2025-09-16 20:10:04 +0100

    Fix: Enregistrement basique de MouvementStock pour débogage

--- stock/admin.py
+++ stock/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from .models import MouvementStock
 
-@admin.register(MouvementStock)
-class MouvementStockAdmin(admin.ModelAdmin):
-    list_display = ('matiere_premiere_id', 'quantite', 'type_mouvement', 'date_mouvement', 'description')
-    # list_filter = ('type_mouvement', 'matiere_premiere')
-    # search_fields = ('matiere_premiere__nom', 'description')
-    # date_hierarchy = 'date_mouvement'
-    # list_select_related = ('matiere_premiere',)
+# Temporairement désactivé pour débogage
+# @admin.register(MouvementStock)
+# class MouvementStockAdmin(admin.ModelAdmin):
+#     list_display = ('matiere_premiere_id', 'quantite', 'type_mouvement', 'date_mouvement', 'description')
+#     # list_filter = ('type_mouvement', 'matiere_premiere')
+#     # search_fields = ('matiere_premiere__nom', 'description')
+#     # date_hierarchy = 'date_mouvement'
+#     # list_select_related = ('matiere_premiere',)
+
+# Enregistrement basique temporaire
+admin.site.register(MouvementStock)
