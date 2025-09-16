4bf8fa54a56c371f97d5a286f94b6586baebcaf1
kira3622 <affifyassine3@gmail.com>
2025-09-16 19:51:59 +0100

    Fix: Simplification de l'admin MouvementStock pour débogage

--- stock/admin.py
+++ stock/admin.py
@@ -3,8 +3,8 @@ from .models import MouvementStock
 
 @admin.register(MouvementStock)
 class MouvementStockAdmin(admin.ModelAdmin):
-    list_display = ('matiere_premiere', 'quantite', 'type_mouvement', 'date_mouvement')
-    list_filter = ('type_mouvement', 'matiere_premiere')
-    search_fields = ('matiere_premiere__nom',)
+    list_display = ('matiere_premiere', 'quantite', 'type_mouvement', 'date_mouvement', 'description')
+    # list_filter = ('type_mouvement', 'matiere_premiere')
+    # search_fields = ('matiere_premiere__nom', 'description')
     # date_hierarchy = 'date_mouvement'
     list_select_related = ('matiere_premiere',)
