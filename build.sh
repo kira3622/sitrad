45c740dd8cacb4dcf9da6372eb071d44a5f3ad13
kira3622 <affifyassine3@gmail.com>
2025-09-16 20:14:10 +0100

    Fix: Correction du script build.sh pour résoudre le problème de migrations manquantes

--- build.sh
+++ build.sh
@@ -2,8 +2,16 @@
 # Exit on error
 set -o errexit
 
+echo "Installing dependencies..."
 pip install -r requirements.txt
 
+echo "Making migrations..."
+python manage.py makemigrations
+
+echo "Running migrations..."
+python manage.py migrate
+
+echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
-python manage.py migrate
+echo "Build completed successfully!"
